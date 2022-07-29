d0f6946948f9263b1cbfd01d6bb07d721303b22c
avishekprasadbarnwal <49061068+avishekprasadbarnwal@users.noreply.github.com>
2022-07-29 10:40:00 +0530

    Add modal complete

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const modalOpenBtn = document.querySelectorAll('.show-modal');
+const modalCloseBtn = document.querySelector('.close-modal');
+
+// console.log(modalOpenBtn.length);
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < modalOpenBtn.length; i++){
+    modalOpenBtn[i].addEventListener('click', openModal);
+};
+
+
+modalCloseBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function(e){
+    console.log(e.key);
+    
+    if(e.key === "Escape" && !modal.classList.contains('hidden')){
+            closeModal();
+    }
+})
